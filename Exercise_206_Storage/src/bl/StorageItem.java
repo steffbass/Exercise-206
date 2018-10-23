ffd93df031e86b65b8889c080cf1e22df75bc24d
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-10-23 09:56:14 +0200

    implemented nearly all functions

--- Exercise_206_Storage/src/bl/StorageItem.java
+++ Exercise_206_Storage/src/bl/StorageItem.java
@@ -11,4 +11,55 @@ package bl;
  */
 public class StorageItem {
     
+    private int id;
+    private String desc;
+    private int place;
+    private int amount;
+
+    public StorageItem(int id, String desc, int place) {
+        this.id = id;
+        this.desc = desc;
+        this.place = place;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+    
+    public String toString()
+    {
+     return String.format("%d is a %s and is located at place %d", id,desc,place);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    
+    
+           
 }
--- Exercise_206_Storage/src/bl/Storage_BL.java
+++ Exercise_206_Storage/src/bl/Storage_BL.java
@@ -5,10 +5,71 @@
  */
 package bl;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author stefan
  */
-public class Storage_BL {
+public class Storage_BL extends AbstractTableModel{
+ 
+   private ArrayList<StorageItem> item = new ArrayList<>();
+    private static String[] colNames = {"ID","Description","Place","Amount"};
+
+    @Override
+    public int getRowCount() {
+     return  item.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+       return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+      StorageItem si = (StorageItem) item.get(rowIndex);
+      return si;
+    }
     
+      @Override
+    public String getColumnName(int i) {
+        return colNames[i];
+    }
+    
+    public void add (StorageItem si)
+    {
+     item.add(si);
+       this.fireTableRowsInserted(item.size()-1, item.size()-1);
+    }
+    
+    public void remove(StorageItem si)
+    {
+     item.remove(si);
+     this.fireTableRowsDeleted(item.size()-1, item.size()-1);
+    }
+    
+    public void sell(StorageItem si,int anz)
+    {
+   int help = si.getAmount();
+   int neu = help-anz;
+   si.setAmount(neu);
+    }
+    
+    public void buy(StorageItem si, int anz)
+    {
+     int help = si.getAmount();
+     int neu = help + anz;
+     si.setAmount(neu);
+    }
+    
+    public void edit(StorageItem si, int id, String desc, int amount, int place)
+    {
+     si.setAmount(amount);
+     si.setDesc(desc);
+     si.setId(id);
+     si.setPlace(place);
+    }
 }
